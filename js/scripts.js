216972b0b3785bc1b8b51f562f1e8245d881ef31
Kevin Buhler <kevinbuhlerr@gmail.com>
2020-07-21 20:32:42 -0600

    fixed welcome image and added changing colors in the navbar

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20) {
             $(".navbar").addClass("sticky");
+
             $(".goTop").fadeIn();
         }
         else {
             $(".navbar").removeClass("sticky");
+
             $(".goTop").fadeOut();
         }
     });
