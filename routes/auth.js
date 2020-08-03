a96bba9005d4bf3947d3e50894b30e53f9bdebe1
Kevin Buhler <kevinbuhlerr@gmail.com>
2020-08-03 15:44:37 -0600

    test link for google auth

--- routes/auth.js
+++ routes/auth.js
@@ -23,6 +23,16 @@ router.get(
 	}
 );
 
+router.get(
+	'https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?response_type=code&redirect_uri=http%3A%2F%2Fenigmatic-ocean-61732.herokuapp.com%2Fauth%2Fgoogle%2Fcallback&scope=profile&client_id=202343364934-hbd445gggs5h5jti06v95a4mrcqc21rg.apps.googleusercontent.com&flowName=GeneralOAuthFlow',
+	passport.authenticate('google', {
+		failureRedirect: '/login',
+	}),
+	(req, res) => {
+		res.redirect('/account');
+	}
+);
+
 // @desc Logout user
 // @route /auth/logout
 router.get('/logout', (req, res) => {
